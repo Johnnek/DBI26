06ac1f46d60d70362fd3812cd74a702921917435
Makapre <m.preikschat@gmail.com>
2017-12-06 14:18:05 +0100

    Version 1
    aktuelle Version

--- TPSDatabase/src/TPSDatabase.java
+++ TPSDatabase/src/TPSDatabase.java
@@ -4,11 +4,46 @@ import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Marius Preikschat und John Parol
+ * 
+ * swtweb.w-hs.de/bobepo
+ * dbi26-dbdfgsi26
+ *
+ */
 
-// Neuer Code
+/**
+ * Klasse Timer zur Zeiterfassung des Benchmarks
+ *
+ */
+class Timer{
+	/**
+	 * Variable, die vom System die Zeit in Millisekunden speichert
+	 */
+	long millis;
+	
+	/**
+	 * Funktion zum Starten der Zeiterfassung
+	 */
+	void start() {
+		millis = System.currentTimeMillis();
+	}
+	
+	/**
+	 * Funktion zum Stoppen der Zeiterfassung
+	 */
+	void stop() {
+		millis = System.currentTimeMillis() - millis;
+		double dmillis = (double)millis/1000;
+		System.out.println(dmillis + " Sekunden");
+	}
+}
+
 public class TPSDatabase {
 
 protected static BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
@@ -39,69 +74,172 @@ protected static BufferedReader stdin = new BufferedReader(new InputStreamReader
 	}
 	//Input finished
 static Connection conn;
+
 	public static void main(String[] args) {
+		
+		Timer t = new Timer();//Timer zur Zeiterfassung für den Benchmark
+		
 		if(args.length != 2) {
 			System.err.println("usage: java jdbcConnect <user><pwd>\n");
 			System.exit(1);
 		}else
 			try {
-			String dbURL = "jdbc:mysql://localhost:3306/tps";
+			String dbURL = "jdbc:mysql://localhost:3306?verifyServerCertificate=false&useSSL=true";
 			conn = getConnection(dbURL, args[0], args[1]);
-			System.out.println("\nConnected to TPS Database!");
+			System.out.println("Connected to DBMS!");
 
+			/**
+			 * 1. Optimierung, aus nicht jede Änderungen an das DBMS gepusht werden, sondern am Ende ein Commit reicht, um die Änderungen zu pushen 
+			 */
 			conn.setAutoCommit(false);
+			
+			ResultSet resultSet = conn.getMetaData().getCatalogs();
 
+			/**
+			 * Überprüft im DBMS, ob die Benchmark Datenbank TPS bereits existiert, wenn ja, dann wird diese gelöscht
+			 */
+		        while (resultSet.next()) {
+
+		          String databaseName = resultSet.getString(1);
+		            if(databaseName.equals("tps")){
+		            	PreparedStatement dropDatabase = conn.prepareStatement(
+								"drop database tps;"
+								);
+		            	dropDatabase.executeUpdate();
+		            	conn.commit();
+		            	System.out.println("Existing Database TPS was dropped!");
+		            }
+		        }
+		        resultSet.close();
+		
+		        /**
+		         * Benchmark Datenbank TPS wird erstellt
+		         */
+				PreparedStatement createDatabase = conn.prepareStatement(
+						"create database tps; "
+						);
+				PreparedStatement useDatabase = conn.prepareStatement(
+						"use tps;"
+						);
+				createDatabase.executeUpdate();
+				useDatabase.executeUpdate();
+				conn.commit();
+				
+				System.out.println("Database TPS created!");
+				
+				/**
+				 * Benchmark Datenbank TPS wird mit den benötigten Tabellen gefüllt
+				 */
+				PreparedStatement createBranches = conn.prepareStatement(
+						"create table branches " + 
+						"( branchid int not null," + 
+						" branchname char(20) not null," + 
+						" balance int not null," + 
+						" address char(72) not null," + 
+						" primary key (branchid) );"
+						);		
+				createBranches.executeUpdate();
+				conn.commit();
+				
+				PreparedStatement createAccounts = conn.prepareStatement(
+						"create table accounts " + 
+						"( accid int not null," + 
+						" name char(20) not null," + 
+						" balance int not null," + 
+						" branchid int not null," + 
+						" address char(68) not null," + 
+						" primary key (accid)," + 
+						" foreign key (branchid) references branches (branchid) );"
+						);
+				createAccounts.executeUpdate();
+				conn.commit();
+				
+				PreparedStatement createTellers = conn.prepareStatement(
+						"create table tellers " + 
+						"( tellerid int not null," + 
+						" tellername char(20) not null," + 
+						" balance int not null," + 
+						" branchid int not null," + 
+						" address char(68) not null," + 
+						" primary key (tellerid)," + 
+						" foreign key (branchid) references branches (branchid) );"
+						);
+				createTellers.executeUpdate();
+				conn.commit();
+				
+				PreparedStatement createHistory = conn.prepareStatement(
+						"create table history " + 
+						"( accid int not null," + 
+						" tellerid int not null," + 
+						" delta int not null," + 
+						" branchid int not null," + 
+						" accbalance int not null," + 
+						" cmmnt char(30) not null," + 
+						" foreign key (accid) references accounts (accid)," + 
+						" foreign key (tellerid) references tellers (tellerid)," + 
+						" foreign key (branchid) references branches (branchid) );"
+						);
+				createHistory.executeUpdate();
+				conn.commit();
+
+			/**
+			 * Eingabe des Parameters n, um den Benchmark durchzuführen
+			 */
 			int n;
-			int i;
-			int rdm;
 			Scanner s = new Scanner(System.in);
 			System.out.println("Bitte n eingeben: ");
 			n = s.nextInt();
 			s.close();
 
-			String branchname = "INGDiBaBankinstitut";
-			String baddress = "Musterstrasse 1, 66666 Musterstadt Nord - Rhein - Westfalen, Deutschland";
-					
-			String accname = "abcdefghijkmnopqrstu";
-			String aaddress = "Musterstrasse 1, 66666 Musterstadt Nord-Rhein-Westfalen, Deutschland";
-					
-			String telname = "abcdeabcdeabcdeabcde";
-			String taddress = "Musterstrasse 1, 66666 Musterstadt Nord-Rhein-Westfalen, Deutschland";
+			String name = "INGDiBaBankinsititut";
+			String branchAddress = "Musterstrasse 1, 66666 Musterstadt Nord - Rhein - Westfalen, Deutschland";
+			String accountsAddress = "Musterstrasse 1, 66666 Musterstadt Nord-Rhein-Westfalen, Deutschland";
 
-			PreparedStatement stmtbranch = conn.prepareStatement(
-					"insert into tps.branches values (?,'" + branchname + "',0,'" + baddress + "');"
+			/**
+			 * 2. Optimierung des Benchmarks, in dem Prepared Statements benutzt werden
+			 */
+			PreparedStatement stmt_branches = conn.prepareStatement(
+					"insert into tps.branches values (?,'" + name + "',0,'" + branchAddress + "');"
 					);
-			PreparedStatement stmtacc = conn.prepareStatement(
-					"insert into tps. accounts values (?,'" + accname + "',0,?,'" + aaddress + "');"
+			PreparedStatement stmt_accounts = conn.prepareStatement(
+					"insert into tps. accounts values (?,'" + name + "',0,?,'" + accountsAddress + "');"
 					);
-			PreparedStatement stmttell = conn.prepareStatement(
-					"insert into tps.tellers values(?,'" + telname + "',0,?,'" + taddress + "');"
+			PreparedStatement stmt_tellers = conn.prepareStatement(
+					"insert into tps.tellers values(?,'" + name + "',0,?,'" + accountsAddress + "');"
 					);
 
-					for(i = 0; i <= n; i++)
-					{
-						stmtbranch.setInt(1,  i);
-						stmtbranch.executeUpdate();
-					}
-					for(i = 0; i <= n*100000; i++)
-					{
-						rdm = (int) (Math.random() * n + 1);
-						stmtacc.setInt(1, i);
-						stmtacc.setInt(2, rdm);
-						stmtacc.executeUpdate();
-					}
-					for(i = 0; i <= n*10; i++)
-					{
-						rdm =(int) (Math.random() * n + 1);
-						stmttell.setInt(1, i);
-						stmttell.setInt(2, rdm);
-						stmttell.executeUpdate();
-					}
-
+			/**
+			 * Start des Timers
+			 */
+			t.start();
+			
+			int i;
+			for(i = 1; i <= n; i++) {
+				stmt_branches.setInt(1, i);
+				stmt_branches.executeUpdate();
+			}
+			for(i = 1; i <= n*100000; i++) {
+				int zufall_BranchID = (int)Math.random() * n + 1;
+				stmt_accounts.setInt(1, i);
+				stmt_accounts.setInt(2, zufall_BranchID);
+				stmt_accounts.executeUpdate();
+			}
+			for(i = 1; i <= n*10; i++) {
+				int zufall_BranchID = (int)Math.random() * n + 1;
+				stmt_tellers.setInt(1, i);
+				stmt_tellers.setInt(2, zufall_BranchID);
+				stmt_tellers.executeUpdate();
+			}
+			
 			conn.commit();
-
-		            conn.close();
-		            System.out.println("Disconnected!");
+			
+			/**
+			 * Stop des Timers
+			 */
+			t.stop();
+			
+			conn.close();
+		    System.out.println("Disconnected!");
 
 		         }
 		         catch (SQLException e)
@@ -112,5 +250,3 @@ static Connection conn;
 	}
 
 }
-
-// Test -> etwas aendern
